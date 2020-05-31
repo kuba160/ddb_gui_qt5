09fbbe41cd4d2b9719e6d5ca715fcc9e7c7b108e
Jakub Wasylków <kuba_160@protonmail.com>
2020-05-31 14:05:49 +0200

    PlaylistBrowser: add missing

---
+++ PlaylistBrowser.cpp
@@ -0,0 +1,78 @@
+#include "PlaylistBrowser.h"
+
+#include <QStyle>
+#include <QtGui>
+
+PlaylistBrowser::PlaylistBrowser(QWidget * parent, DBApi *Api) : QListWidget(parent), DBToolbarWidget(parent, Api) {
+    // Fill list
+    int count = api->getPlaylistCount();
+    for (int i = 0; i < count; i++) {
+        addItem(api->playlistNameByIdx(i));
+    }
+    setCurrentRow(api->deadbeef->plt_get_curr_idx());
+
+    // Connections
+    connect (this, SIGNAL(itemClicked(QListWidgetItem *)), this, SLOT(onItemClicked(QListWidgetItem *)));
+    connect (this, SIGNAL(itemPressed(QListWidgetItem *)), this, SLOT(onItemClicked(QListWidgetItem *)));
+    connect (this, SIGNAL(playlistSelected(int)), api, SLOT(changePlaylist(int)));
+    connect (api, SIGNAL(playlistChanged(int)), this, SLOT(selectPlaylist(int)));
+    connect (api, SIGNAL(playlistMoved(int, int)), this, SLOT(playlistOrderChanged(int, int)));
+
+    // Stylesheet
+    this->setStyleSheet(QString("QListView::item {padding: 5px}"));
+
+    // Drag'n'drop
+    this->setDragEnabled(true);
+    setDragDropMode(QAbstractItemView::InternalMove);
+}
+
+void PlaylistBrowser::mousePressEvent(QMouseEvent *event) {
+    if (event->button() == Qt::LeftButton) {
+        QListWidget::mousePressEvent(event);
+    }
+    else {
+        event->accept();
+        return;
+    }
+}
+
+QDockWidget *PlaylistBrowser::constructorDockWidget(QWidget *parent, DBApi *Api) {
+    QWidget *pb = new PlaylistBrowser(parent, Api);
+    QDockWidget *dw = new QDockWidget(QString("Playlist Browser"));
+
+    dw->setWidget(pb);
+    return dw;
+}
+
+void PlaylistBrowser::selectPlaylist(int pl) {
+    setCurrentItem(item(pl));
+}
+
+void PlaylistBrowser::onItemClicked(QListWidgetItem *item) {
+    emit playlistSelected(row(item));
+}
+
+void PlaylistBrowser::dropEvent(QDropEvent *event) {
+    QListWidgetItem *it = currentItem();
+    int pos_before = row(it);
+    QListWidget::dropEvent(event);
+    int pos_after = row(it);
+
+    if (pos_before != pos_after) {
+        // move happened
+        our_pl = pos_before;
+        our_before = pos_after;
+        api->movePlaylist(pos_before, pos_after);
+    }
+}
+
+void PlaylistBrowser::playlistOrderChanged(int pl, int before) {
+    if (pl == our_pl && before == our_before) {
+        // Ignore order change done by this widget
+        our_pl = -1;
+        our_before = -1;
+        return;
+    }
+    QListWidgetItem *currentItem = takeItem(pl);
+    insertItem(before, currentItem);
+}
---
+++ PlaylistBrowser.h
@@ -0,0 +1,41 @@
+#ifndef PLAYLISTBROWSER_H
+#define PLAYLISTBROWSER_H
+
+#include <QWidget>
+#include <QListWidget>
+#include <QMouseEvent>
+#include "DBApi.h"
+
+class PlaylistBrowser : public QListWidget, public DBToolbarWidget
+{
+    Q_OBJECT
+public:
+    PlaylistBrowser(QWidget *parent = nullptr, DBApi *Api = nullptr);
+
+    static QDockWidget *constructorDockWidget(QWidget *parent = nullptr, DBApi *Api = nullptr);
+
+private slots:
+    void onItemClicked(QListWidgetItem *);
+
+private:
+    void dropEvent(QDropEvent *);
+
+    // used to detect if move event comes from this widget
+    int our_pl = -1;
+    int our_before = -1;
+
+protected slots:
+    void mousePressEvent(QMouseEvent *ev);
+
+signals:
+    // playlist got selected by this widget
+    void playlistSelected(int);
+
+public slots:
+    // select playlist in this widget
+    void selectPlaylist(int);
+    // move playlist pl to before
+    void playlistOrderChanged(int pl, int before);
+};
+
+#endif // PLAYLISTBROWSER_H
