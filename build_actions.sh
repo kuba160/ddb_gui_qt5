718fd8e56e29e928984bcb078fe6b88c5079f791
Jakub Wasylków <kuba_160@protonmail.com>
2021-04-15 19:42:52 +0200

    11

--- build_actions.sh
+++ build_actions.sh
@@ -1,17 +1,17 @@
 set -x
-echo $OSTYPE
-case "$OSTYPE" in
-  darwin*)
+echo $TRAVIS_OS_NAME
+case "$TRAVIS_OS_NAME" in
+  osx)
 	brew install qt@5
 	export PATH="/usr/local/opt/qt@5/bin:$PATH"
 	export LDFLAGS="-L/usr/local/opt/qt@5/lib"
 	export CPPFLAGS="-I/usr/local/opt/qt@5/include"
 	export PKG_CONFIG_PATH="/usr/local/opt/qt@5/lib/pkgconfig"
 	;; 
-  linux*)
+  linux)
 	apt-get install -y qt5-default
 	;;
-  msys*)
+  windows)
 	export QMAKE_PREFIX=/mingw64/qt5-static/bin/
 	pacman --needed --noconfirm -S mingw-w64-x86_64-qt5-static
 	;;
