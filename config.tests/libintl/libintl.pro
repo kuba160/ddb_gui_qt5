85b32bf16f7fdf8564ff3101d696f92681d161e4
Jakub Wasylków <kuba_160@protonmail.com>
2023-09-10 17:00:51 +0200

    cleanup

--- config.tests/libintl/libintl.pro
+++
@@ -1,7 +0,0 @@
-TEMPLATE = app
-QT = 
-SOURCES += test_libintl.cpp
-
-win32 {
-	LIBS += -lintl
-}
--- config.tests/libintl/test_libintl.cpp
+++
@@ -1,8 +0,0 @@
-#include <libintl.h>
-#include <stdio.h>
-
-int main() {
-	bind_textdomain_codeset ("hello", "");
-	printf(gettext("Hello World!"));
-	return 0;
-}
